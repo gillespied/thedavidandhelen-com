d5da22d5efe1f50520b039fb2a3529b355459ba3
David Gillespie <david@espied.tech>
2019-04-09 20:33:30 +0100

    fixed formatting of confirmation pages

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -7,10 +7,12 @@ import SEO from '../components/seo';
 const Thanks = () => (
   <Layout>
     <SEO title="Thanks" />
-    <div className="container maincontent">
-      <h1>Thanks</h1>
-      <p>We have received your message and will get back you asap.</p>
-      <Link to="/">Go back to the homepage</Link>
+    <div className="section">
+      <div className="container">
+        <h1>Thanks</h1>
+        <p>We have received your message and will get back you asap.</p>
+        <Link to="/">Go back to the homepage</Link>
+      </div>
     </div>
   </Layout>
 );
